37116ff3247152c0959c7b477a47c0f50b361010
EthanMiller758 <138612628+EthanMiller758@users.noreply.github.com>
2024-04-02 13:47:00 -0230

    Delete DDL/CREATE_TABLE_Customers.sql

--- DDL/CREATE_TABLE_Customers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE public."Customers" (
-    customerID SERIAL PRIMARY KEY,
-    firstName VARCHAR(255),
-    lastName VARCHAR(255),
-    addressID INT REFERENCES public."Address"(addressID),
-    email VARCHAR(255),
-    phone VARCHAR(20)
-);
