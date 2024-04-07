85877bd856a4ba9e8d1feb87bbc707b7c90f5de1
Evan <evan.harte@keyin.com>
2024-04-07 17:37:27 -0230

    Update game search functionality (postgres) to include genre, platform, and quantity in search results

--- server/services/pg.fulltext.dal.js
+++ server/services/pg.fulltext.dal.js
@@ -3,9 +3,14 @@ const dal = require("./postgres_db");
 var getFullTextPG = function (text) {
   if (DEBUG) console.log("pg.dal.getFullTextPG()");
   return new Promise(function (resolve, reject) {
-    const sql = `SELECT gametitle, description, price, instorelocation FROM "Games"
+    const sql = `SELECT gametitle, description, price, instorelocation, genrename, consolename, quantity FROM "Games"
+        JOIN "Genres" ON "Games".genreid = "Genres".genreid
+        JOIN "Consoles" ON "Games".consoleid = "Consoles".consoleid
+        JOIN "GamesConsoles" ON "Games".gameid = "GamesConsoles".gameid
+        JOIN "Inventory" ON "GamesConsoles".gamesconsoleid = "Inventory".gamesconsoleid
         WHERE description iLIKE '%'||$1||'%'
         OR gametitle iLIKE '%'||$1||'%'`;
+
     dal.query(sql, [text], (err, result) => {
       if (err) {
         // logging should go here
