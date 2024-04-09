672192d4ce88a0e25e3731b46bbcb27e23c43f55
EthanMiller758 <138612628+EthanMiller758@users.noreply.github.com>
2024-04-09 13:36:49 -0230

    Here is my test file for the index.js file.

---
+++ server/index.test.js
@@ -0,0 +1,14 @@
+const { checkAuthenticated } = require('./index');
+
+jest.mock('./index', () => ({
+    checkAuthenticated: jest.fn(() => true),
+}));
+
+describe('Test login function', () => {
+    it('should return true if login is successful', () => {
+        const result = checkAuthenticated();
+
+        expect(result).toBe(true);
+    });
+});
+
