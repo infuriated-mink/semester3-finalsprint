bf8b9f5cebdab70f8969f952d17cc3aac309cc1e
Vanessa Rice <vanessa.rice@keyin.com>
2024-04-09 11:07:34 -0230

    All files merged upto logging step.

--- server/index.js
+++ server/index.js
@@ -3,88 +3,79 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const app = express();
 const bcrypt = require("bcrypt");
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const jwt = require("jsonwebtoken");
+const LocalStrategy = require("passport-local").Strategy;
 const flash = require("express-flash");
 const session = require("express-session");
 const methodOverride = require("method-override");
 const fs = require("fs");
+
+const app = express();
 const PORT = 3000;
+global.DEBUG = true;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(methodOverride("_method"));
 
 let users = [];
 fs.readFile("./models/users.json", (err, data) => {
-  if (err) {
-    console.log("Error reading users.json:", err);
-  } else {
-    users = JSON.parse(data);
-  }
+  if (err) throw err;
+  users = JSON.parse(data);
 });
 
 function saveUsers() {
-  fs.writeFile("./models/users.json", JSON.stringify(users), (err) => {
-    if (err) {
-      console.log("Error writing users.json:", err);
-    }
+  fs.writeFile("./models/users.json", JSON.stringify(users, null, 2), err => {
+    if (err) throw err;
   });
 }
 
-app.use(express.urlencoded({ extended: false }));
+passport.use(new LocalStrategy({ usernameField: "email" }, async (email, password, done) => {
+  const user = users.find(user => user.email === email);
+  if (!user) {
+    return done(null, false, { message: "No user with that email" });
+  }
 
-passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "email",
-    },
-    async (email, password, done) => {
-      try {
-        const user = users.find((user) => user.email === email);
-
-        if (!user) {
-          return done(null, false, { message: "User not found" });
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-
-        if (isMatch) {
-          const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-            expiresIn: "1h",
-          });
-          return done(null, user, { token });
-        } else {
-          return done(null, false, { message: "Incorrect password" });
-        }
-      } catch (error) {
-        return done(error);
-      }
+  try {
+    if (await bcrypt.compare(password, user.password)) {
+      return done(null, user);
+    } else {
+      return done(null, false, { message: "Password incorrect" });
     }
-  )
-);
+  } catch (e) {
+    return done(e);
+  }
+}));
 
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
+passport.serializeUser((user, done) => done(null, user.id));
 passport.deserializeUser((id, done) => {
-  const user = users.find((user) => user.id === id);
+  const user = users.find(user => user.id === id);
   done(null, user);
 });
 
-app.use(passport.initialize());
+function checkAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
+}
 
-app.set("view-engine", "ejs");
-app.use(flash());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
-app.use(passport.session());
-app.use(methodOverride("_method"));
+function checkNotAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect("/");
+  }
+  next();
+}
 
 app.get("/", checkAuthenticated, (req, res) => {
   res.render("index.ejs", { name: req.user.name });
@@ -94,25 +85,11 @@ app.get("/login", checkNotAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
-app.post("/login", (req, res) => {
-  const user = users.find((user) => user.email === req.body.email);
-  if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET);
-    res.render("index.ejs", { name: user.name, token });
-  } else {
-    res.status(401).json({ message: "Invalid credentials" });
-  }
-});
-
-app.post(
-  "/login",
-  checkNotAuthenticated,
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
+app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true
+}));
 
 app.get("/register", checkNotAuthenticated, (req, res) => {
   res.render("register.ejs");
@@ -121,12 +98,13 @@ app.get("/register", checkNotAuthenticated, (req, res) => {
 app.post("/register", checkNotAuthenticated, async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
+    const newUser = {
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
-      password: hashedPassword,
-    });
+      password: hashedPassword
+    };
+    users.push(newUser);
     saveUsers();
     res.redirect("/login");
   } catch {
@@ -135,34 +113,20 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
 });
 
 app.delete("/logout", (req, res) => {
-  req.logOut();
-  res.redirect("/login");
+  req.logOut(() => {
+    res.redirect("/login");
+  });
 });
 
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
+// "/search" route setup
+const searchRouter = require("./routes/search");
+app.use("/search", checkAuthenticated, searchRouter);
 
-  res.redirect("/login");
-}
+// Handling 404
+app.use((req, res) => {
+  res.status(404).render("404");
+});
 
-app.get(
-  "/protected",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    res.json({ message: "You have accessed a protected route" });
-  }
-);
-
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return res.redirect("/");
-  }
-  next();
-}
-
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
