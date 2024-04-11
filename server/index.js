6ad2487d8c95ac90d7448f501b4123a5d82e91f6
Vanessa Rice <vanessa.rice@keyin.com>
2024-04-10 22:42:23 -0230

    added login, nav, and register css

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const flash = require("express-flash");
 const session = require("express-session");
 const methodOverride = require("method-override");
 const fs = require("fs");
-const initializePassport = require("./passport-config");
+const initializePassport = require('./passport-config');
 initializePassport(passport, getUserByEmail, getUserById);
 
 const app = express();
@@ -18,14 +18,13 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 app.use(flash());
-app.use(
-  session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-  })
-);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride("_method"));
@@ -37,11 +36,11 @@ fs.readFile("./models/users.json", (err, data) => {
 });
 
 function getUserByEmail(email) {
-  return users.find((user) => user.email === email);
+  return users.find(user => user.email === email);
 }
 
 function getUserById(id) {
-  return users.find((user) => user.id === id);
+  return users.find(user => user.id === id);
 }
 
 initializePassport(passport, getUserByEmail, getUserById);
@@ -57,13 +56,13 @@ function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect("/login");
+  res.redirect('/login');
 }
 
 // Middleware to check if the user is not authenticated
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
-    return res.redirect("/");
+    return res.redirect('/');
   }
   next();
 }
@@ -76,15 +75,11 @@ app.get("/login", checkNotAuthenticated, (req, res) => {
   res.render("login.ejs");
 });
 
-app.post(
-  "/login",
-  checkNotAuthenticated,
-  passport.authenticate("local", {
-    successRedirect: "/",
-    failureRedirect: "/login",
-    failureFlash: true,
-  })
-);
+app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+  successRedirect: "/",
+  failureRedirect: "/login",
+  failureFlash: true
+}));
 
 app.get("/register", checkNotAuthenticated, (req, res) => {
   res.render("register.ejs");
@@ -97,7 +92,7 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
       id: Date.now().toString(),
       name: req.body.name,
       email: req.body.email,
-      password: hashedPassword,
+      password: hashedPassword
     };
     users.push(newUser);
     saveUsers();
@@ -107,46 +102,15 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
   }
 });
 
-app.post("/login", function (req, res, next) {
-  passport.authenticate("local", function (err, user, info) {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.redirect("/login");
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      return res.redirect("/models/users/" + user.username);
-    });
-  })(req, res, next);
-});
-
-// Logout route
-app.post("/logout", function (req, res, next) {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/login");
-  });
-});
-
 app.delete("/logout", (req, res) => {
-  req.logout(function (err) {
-    if (err) {
-      return next(err);
-    }
-    // Render the loggedOut.ejs view after logging out
+  req.logOut(() => {
     res.redirect("/login");
   });
 });
 
 // Function to save users to the JSON file
 function saveUsers() {
-  fs.writeFile("./models/users.json", JSON.stringify(users, null, 2), (err) => {
+  fs.writeFile("./models/users.json", JSON.stringify(users, null, 2), err => {
     if (err) throw err;
   });
 }
@@ -160,10 +124,8 @@ app.use((req, res) => {
   res.status(404).render("404");
 });
 
-if (process.env.NODE_ENV !== "test") {
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
 
 module.exports = app;
